9ac792998588f1582e42bee1ce7c88e2177060db
Kaarthik S Kumar <70267161+kaarthi1997@users.noreply.github.com>
2020-09-01 16:46:11 +0530

    user page design

---
+++ user_winform/user_winform/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace user_winform
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_user = new System.Windows.Forms.Label();
+            this.lbl_category = new System.Windows.Forms.Label();
+            this.lbl_username = new System.Windows.Forms.Label();
+            this.lbl_password = new System.Windows.Forms.Label();
+            this.txt_username = new System.Windows.Forms.TextBox();
+            this.txt_password = new System.Windows.Forms.TextBox();
+            this.cb_user = new System.Windows.Forms.ComboBox();
+            this.cb_category = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // lbl_user
+            // 
+            this.lbl_user.AutoSize = true;
+            this.lbl_user.Location = new System.Drawing.Point(117, 69);
+            this.lbl_user.Name = "lbl_user";
+            this.lbl_user.Size = new System.Drawing.Size(85, 17);
+            this.lbl_user.TabIndex = 0;
+            this.lbl_user.Text = "Select User:";
+            // 
+            // lbl_category
+            // 
+            this.lbl_category.AutoSize = true;
+            this.lbl_category.Location = new System.Drawing.Point(117, 104);
+            this.lbl_category.Name = "lbl_category";
+            this.lbl_category.Size = new System.Drawing.Size(69, 17);
+            this.lbl_category.TabIndex = 1;
+            this.lbl_category.Text = "Category:";
+            // 
+            // lbl_username
+            // 
+            this.lbl_username.AutoSize = true;
+            this.lbl_username.Location = new System.Drawing.Point(117, 141);
+            this.lbl_username.Name = "lbl_username";
+            this.lbl_username.Size = new System.Drawing.Size(83, 17);
+            this.lbl_username.TabIndex = 2;
+            this.lbl_username.Text = "User Name:";
+            // 
+            // lbl_password
+            // 
+            this.lbl_password.AutoSize = true;
+            this.lbl_password.Location = new System.Drawing.Point(117, 174);
+            this.lbl_password.Name = "lbl_password";
+            this.lbl_password.Size = new System.Drawing.Size(73, 17);
+            this.lbl_password.TabIndex = 3;
+            this.lbl_password.Text = "Password:";
+            // 
+            // txt_username
+            // 
+            this.txt_username.Location = new System.Drawing.Point(186, 141);
+            this.txt_username.Name = "txt_username";
+            this.txt_username.Size = new System.Drawing.Size(121, 22);
+            this.txt_username.TabIndex = 6;
+            // 
+            // txt_password
+            // 
+            this.txt_password.Location = new System.Drawing.Point(186, 174);
+            this.txt_password.Name = "txt_password";
+            this.txt_password.Size = new System.Drawing.Size(121, 22);
+            this.txt_password.TabIndex = 7;
+            // 
+            // cb_user
+            // 
+            this.cb_user.FormattingEnabled = true;
+            this.cb_user.Location = new System.Drawing.Point(208, 69);
+            this.cb_user.Name = "cb_user";
+            this.cb_user.Size = new System.Drawing.Size(121, 24);
+            this.cb_user.TabIndex = 8;
+            // 
+            // cb_category
+            // 
+            this.cb_category.FormattingEnabled = true;
+            this.cb_category.Location = new System.Drawing.Point(186, 104);
+            this.cb_category.Name = "cb_category";
+            this.cb_category.Size = new System.Drawing.Size(121, 24);
+            this.cb_category.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.cb_category);
+            this.Controls.Add(this.cb_user);
+            this.Controls.Add(this.txt_password);
+            this.Controls.Add(this.txt_username);
+            this.Controls.Add(this.lbl_password);
+            this.Controls.Add(this.lbl_username);
+            this.Controls.Add(this.lbl_category);
+            this.Controls.Add(this.lbl_user);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_user;
+        private System.Windows.Forms.Label lbl_category;
+        private System.Windows.Forms.Label lbl_username;
+        private System.Windows.Forms.Label lbl_password;
+        private System.Windows.Forms.TextBox txt_username;
+        private System.Windows.Forms.TextBox txt_password;
+        private System.Windows.Forms.ComboBox cb_user;
+        private System.Windows.Forms.ComboBox cb_category;
+    }
+}
+
